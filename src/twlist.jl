b8a3cc9a2403fac10a200cad0322d8aa62f3bd63
Tony Fong <tony.hf.fong@gmail.com>
2014-12-30 17:45:14 +0700

    nested list and examples

--- src/twlist.jl
+++ src/twlist.jl
@@ -5,6 +5,8 @@
 # to reflect their locations in the main pad.
 # This is bad!
 
+using Lazy
+
 type TwListData
     horizontal::Bool
     widgets::Array{TwObj,1} # this is static.
@@ -52,7 +54,7 @@ function newTwList( scr::TwScreen,
     obj
 end
 
-function push_widget!( o::TwObj, w::TwObj )
+function push_widget!( o::TwObj{TwListData}, w::TwObj )
     global rootwin
     # change the widget's window to reflect its location on the canvas
     begx = 0
@@ -86,15 +88,10 @@ function push_widget!( o::TwObj, w::TwObj )
 
     w.window = TwWindow( WeakRef( o ), begy, begx, w.height, w.width )
     push!( o.data.widgets, w )
-    if o.data.focus == 0
-        w.hasFocus = true
-        o.data.focus = 1
-    else
-        w.hasFocus = false
-    end
+    w.hasFocus = false
 end
 
-function update_list_canvas( o::TwObj )
+function update_list_canvas( o::TwObj{TwListData} )
     ws = o.data.widgets
     if isempty( ws )
         # TODO: reconsider this
@@ -112,6 +109,10 @@ function update_list_canvas( o::TwObj )
 end
 
 function draw( o::TwObj{TwListData} )
+    if typeof( o.window ) <: Ptr
+        set_default_focus( o )
+    end
+
     for w in o.data.widgets
         # TODO: no need to draw widget outside visible range? or just draw everything?
         if w.isVisible
@@ -134,6 +135,30 @@ function draw( o::TwObj{TwListData} )
             borderSizeV, borderSizeH,
             min( contentheight-1, canvasheight - o.data.canvaslocy - 1),
             min( contentwidth -1, canvaswidth  - o.data.canvaslocx - 1) )
+    end
+end
+
+function lowest_widget( o::TwObj{ TwListData } )
+    if o.data.focus == 0
+        error( "cannot locate focused widget")
+    end
+    w = o.data.widgets[ o.data.focus ]
+    if objtype( w ) == :List
+        return lowest_widget( w )
+    else
+        return w
+    end
+end
+
+function lowest_widget_location_area( o::TwObj{ TwListData }, y::Int=0, x::Int=0 )
+    if o.data.focus == 0
+        error( "cannot locate focused widget")
+    end
+    w = o.data.widgets[ o.data.focus ]
+    if objtype( w ) == :List
+        return lowest_widget_location_area( w, y+w.window.yloc, x+w.window.xloc )
+    else
+        return ( w, y+w.window.yloc, x+w.window.xloc, w.window.height, w.window.width )
     end
 end
 
@@ -196,15 +221,11 @@ function inject( o::TwObj{TwListData}, token::Any )
                 end
                 for i in r
                     if check_accept_focus(w.data.widgets[i], stepsign )
-                        w.data.focus = i
-                        w.hasFocus = true
                         return true
                     end
                 end
                 return false
             else
-                w.hasFocus = true
-                # make sure canvas is adjusted to show this new widget
                 ensure_visible_on_canvas( w )
                 return true
             end
@@ -217,44 +238,80 @@ function inject( o::TwObj{TwListData}, token::Any )
     # TODO: arrow keys: manhattan distance
     # TODO: mouse: manhattan distance
     if token in [ :tab, :shift_tab ]
-        o.data.widgets[focus].hasFocus = false
-        o.data.focus = 0
+        prevw = o.data.widgets[focus]
         # note that if the widget is a list and can take a tab/shift tab as, we
         # wouldn't be here in the first place
         dorefresh = true
-        if retcode == :pass
-            if token == :tab
-                stp = 1
-            else
-                stp = -1
+        if token == :tab
+            stp = 1
+        else
+            stp = -1
+        end
+        if isrootlist # wrap around
+            i = mod1(focus + stp,length(o.data.widgets) )
+            while (i != focus)
+                w = o.data.widgets[i]
+                if check_accept_focus(w,stp)
+                    deep_unfocus( prevw )
+                    deep_focus( w, stp == -1 ) # 2nd arg is reverse
+                    retcode = :got_it
+                    break
+                end
+                i = mod1(i+ stp,length(o.data.widgets))
             end
-            if isrootlist # wrap around
-                i = mod1(focus + stp,length(o.data.widgets) )
-                while (i != focus)
-                    w = o.data.widgets[i]
-                    if check_accept_focus(w,stp)
-                        o.data.focus = i
-                        retcode = :got_it
-                        break
-                    end
-                    i = mod1(i+ stp,length(o.data.widgets))
-                end
+        else
+            i = focus + stp
+            if stp == 1
+                r = (focus+stp):length(o.data.widgets)
             else
-                i = focus + stp
-                if stp == 1
-                    r = (focus+stp):length(o.data.widgets)
-                else
-                    r = (focus+stp):-1:1
-                end
-                for i in r
-                    w = o.data.widgets[i]
-                    if check_accept_focus(w,stp)
-                        o.data.focus = i
-                        retcode = :got_it
-                        break
-                    end
+                r = (focus+stp):-1:1
+            end
+            for i in r
+                w = o.data.widgets[i]
+                if check_accept_focus(w,stp)
+                    deep_unfocus( prevw )
+                    deep_focus( w, stp == -1 ) # 2nd arg is reverse
+                    retcode = :got_it
+                    break
                 end
             end
+        end
+    elseif token in [ :left, :right, :up, :down,
+        :ctrl_left, :ctrl_right, :ctrl_up, :ctrl_down ] && isrootlist
+        # find the location of the current focus
+        (w, yloc,xloc, height, width) = lowest_widget_location_area( o )
+        if token in [ :up, :ctrl_up ]
+            distfunc = function( to::(Int,Int,Int,Int) )
+                updown_arrow_distance( to, (yloc,xloc,height,width), 1 )
+            end
+        elseif token in [ :down, :ctrl_down ]
+            distfunc = function( to::(Int,Int,Int,Int) )
+                updown_arrow_distance( to, (yloc,xloc,height,width), -1 )
+            end
+        elseif token in [ :left, :ctrl_left ]
+            distfunc = function( to::(Int,Int,Int,Int) )
+                leftright_arrow_distance( to, (yloc,xloc,height,width), 1 )
+            end
+        else
+            distfunc = function( to::(Int,Int,Int,Int) )
+                leftright_arrow_distance( to, (yloc,xloc,height,width), -1 )
+            end
+        end
+        wdists = Any[]
+        geometric_filter( o, distfunc, 0, 0, wdists, true, 40 )
+        candidate = nothing
+        mindist = 999999
+        for (cw,dist) in wdists
+            if dist < mindist
+                candidate = cw
+                mindist = dist
+            end
+        end
+        if candidate != nothing
+            deep_unfocus( w )
+            deep_focus( candidate )
+            dorefresh = true
+            retcode = :got_it
         end
     end
 
@@ -262,4 +319,142 @@ function inject( o::TwObj{TwListData}, token::Any )
         refresh( o )
     end
     return retcode
+end
+
+function deep_unfocus( w::TwObj )
+    tmpw = w
+    while typeof( tmpw ) <: TwObj{ TwListData }
+        focus = tmpw.data.focus
+        tmpw2 = tmpw.data.widgets[focus]
+        tmpw.data.focus = 0
+        tmpw2.hasFocus = false
+        tmpw = tmpw2
+    end
+
+    par::TwObj{TwListData}
+    w.hasFocus = false
+    tmpw = w
+    @oncethen while( !( typeof( tmpw.window ) <: Ptr ) )
+        par = tmpw.window.parent.value
+        par.data.focus = 0
+        par.hasFocus = false
+        tmpw = par
+    end
+end
+
+function set_default_focus( w::TwObj{TwListData}, rev=false )
+    if w.data.focus == 0 && !isempty( w.data.widgets )
+        if rev
+            w.data.focus = length(w.data.widgets)
+        else
+            w.data.focus = 1
+        end
+
+        subw = w.data.widgets[w.data.focus]
+        subw.hasFocus = true
+        if typeof( subw ) <: TwObj{TwListData}
+            set_default_focus( subw, rev )
+        end
+    end
+end
+
+function deep_focus( w::TwObj, rev=false )
+    par::TwObj{TwListData}
+    w.hasFocus = true
+    if objtype(w) == :List
+        set_default_focus( w, rev )
+    end
+    tmpw = w
+    @oncethen while( !( typeof( tmpw.window ) <: Ptr ) )
+        par = tmpw.window.parent.value
+        for (i,c) in enumerate( par.data.widgets )
+            if c == tmpw
+                par.data.focus = i
+                par.hasFocus = true
+                tmpw = par
+                break
+            end
+        end
+        @assert par.data.focus != 0
+        @assert par.hasFocus
+    end
+end
+
+function geometric_filter( o::TwObj{TwListData}, distfunc::Function,
+        y::Int, x::Int, list::Array, excludezero::Bool=false, cutoff::Int=100 )
+    for w in o.data.widgets
+        if objtype( w ) == :List
+            geometric_filter( w, distfunc, y+w.window.yloc, x+w.window.xloc,
+                list, excludezero, cutoff )
+        elseif w.isVisible && w.acceptsFocus
+            to = ( y+w.window.yloc, x+w.window.xloc, w.window.height, w.window.width )
+            dist = distfunc( to )
+            if dist > cutoff || dist == 0 && excludezero
+                continue
+            end
+            push!( list, (w, dist) )
+        end
+    end
+end
+
+function updown_arrow_distance( to::(Int,Int,Int,Int), from::(Int,Int,Int,Int), sgn::Int )
+    tocentx = to[2] + to[4] >> 1
+    tocenty = to[1] + to[3] >> 1
+
+    fromcentx = from[2] + from[4] >> 1
+    fromcenty = from[1] + from[3] >> 1
+
+    if sgn*tocenty >= sgn*fromcenty
+        return 99999
+    end
+
+    ret = (fromcenty - tocenty)*sgn
+
+    if from[2] > tocentx
+        ret += ( from[2] - tocentx ) * 5
+    elseif tocentx >= from[2]+from[4]
+        ret += (tocentx - (from[2]+from[4])+1) * 5
+    end
+    return ret
+end
+
+function leftright_arrow_distance( to::(Int,Int,Int,Int), from::(Int,Int,Int,Int),sgn::Int )
+    tocentx = to[2] + to[4] >> 1
+    tocenty = to[1] + to[3] >> 1
+
+    fromcentx = from[2] + from[4] >> 1
+    fromcenty = from[1] + from[3] >> 1
+
+    if sgn*tocentx >= sgn*fromcentx
+        return 99999
+    end
+
+    ret = (fromcentx - tocentx)*sgn
+
+    if from[1] > tocenty
+        ret += ( from[1] - tocenty ) * 5
+    elseif tocenty >= from[1]+from[3]
+        ret += (tocenty - (from[1]+from[3])+1) * 5
+    end
+    return ret
+end
+
+# used for mouse click
+# inside the area it returns zero, otherwise it's manhattan distance to the boundary
+function point_from_area( y::Int, x::Int, from::(Int,Int,Int,Int) )
+    xdist = 0
+    if x < from[2]
+        xdist += from[2]-x
+    elseif x >= from[2]+from[4]
+        xdist += x - (from[2]+from[4]-1)
+    end
+
+    ydist = 0
+    if y < from[1]
+        ydist += from[1]-y
+    elseif y >= from[1]+from[3]
+        ydist += y - (from[1]+from[3]-1)
+    end
+
+    return xdist + ydist
 end
--- test/list.jl
+++ test/list.jl
@@ -3,14 +3,26 @@ using TermWin
 TermWin.logstart()
 TermWin.initsession()
 #s = "a test"
-v = newTwList( rootTwScreen, 25,80, :random, :random; box=true )
-v.title = "Input: "
+v1 = newTwList( rootTwScreen, 25,80, :random, :random; box=true )
 a = newTwEntry( rootTwScreen, Rational{Int}, 25, :random, :random, title="test1" )
 a.value = 0
-push_widget!( v, a )
+push_widget!( v1, a )
 a = newTwEntry( rootTwScreen, Rational{Int}, 25, :random, :random, title="test2" )
 a.value = 1
-push_widget!( v, a )
+push_widget!( v1, a )
+
+v2 = newTwList( rootTwScreen, 25,80, :random, :random; box=true )
+a = newTwEntry( rootTwScreen, UTF8String, 25, :random, :random, title="test3" )
+a.value = "test"
+push_widget!( v2, a )
+a = newTwEntry( rootTwScreen, Int, 25, :random, :random, title="test4" )
+a.value = 1
+push_widget!( v2, a )
+
+v = newTwList( rootTwScreen, 25, 80, :random, :random; box=true, horizontal=true )
+push_widget!( v, v1 )
+push_widget!( v, v2 )
+
 activateTwObj( rootTwScreen )
 #unregisterTwObj( rootTwScreen, v )
 TermWin.endsession()
