ab8e84fe1044ab2c060e4fb678b610eaa1f17632
Tony Fong <tony.hf.fong@gmail.com>
2026-04-25 20:05:51 -0400

    tshow path bugfix

--- src/TermWin.jl
+++ src/TermWin.jl
@@ -227,7 +227,7 @@ function tshow_(x::T; kwargs...) where {T<:AbstractString}
     # "path:/some/dir" prefix opens the file browser
     # we put a cap in the length of the possible path. isdir will throw anyway if the string is malformed / too long
     s = string(x)
-    if nodeswith(s, "path:") && length(s) < 400 && isdir(s[6:end])
+    if startswith(s, "path:") && length(s) < 400 && isdir(s[6:end])
         return newTwFileBrowser(rootTwScreen, s[6:end]; title = s[6:end])
     end
     pos = :center
