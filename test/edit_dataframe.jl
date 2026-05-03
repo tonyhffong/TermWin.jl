2c34eda73142773bae438330635e1fb16692bbcf
Tony Fong <tony.hf.fong@gmail.com>
2026-05-02 20:52:49 -0400

    improve explanations of using editable dataframe widgets

--- test/edit_dataframe.jl
+++ test/edit_dataframe.jl
@@ -1,8 +1,14 @@
 using TermWin, DataFrames, Dates
+#An example to make an editable table
+
+#It is mandatory to call allowmissing first if any of the column could have missing values
 df = allowmissing( DataFrame(name=["Alice","Bob","Charlie"], score=[3.14,2.71,1.41],
                  grade=["A","B","A"], hired=[Date(2024,1,1),Date(2024,2,1),Date(2024,3,1)],
                  fired=[missing, missing, Date( 2026,1,1) ]
                 ) )
+# Argument order for TwEditTableCol:
+# Name as the symbol, Header string, width, editable, DataType, enumvalues, missingok
+# DataType: no need to do Union with Missing
 cols = [TwEditTableCol(:name,"Name",14,true,String,nothing, false),
         TwEditTableCol(:score,"Score",10,true,Float64,nothing, true),
         TwEditTableCol(:grade,"Grade",8,true,String,["A","B","C"], true),
